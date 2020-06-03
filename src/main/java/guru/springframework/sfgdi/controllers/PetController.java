919da61a4a3b64eccb932f848a0129d41bb2b665
steinko <stein_korsveien@msn.com>
2020-06-03 07:30:07 +0200

    first commit

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by jt on 12/28/19.
  */
 public class PetController {
-
+    @Autowired
     private final PetService petService;
 
     public String whichPetIsTheBest(){
